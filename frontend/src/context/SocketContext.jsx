04d2086280f1330ce13d5dd1bb7efd9c494e078d
yassin-ben-haj-ali <yassinbenhajali5@gmail.com>
2024-09-29 22:46:51 +0100

    fix socket production server url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -21,7 +21,7 @@ export const SocketContextProvider=({children})=>{
     useEffect(()=>{
         if(authUser){
             
-            const socket = io("http://localhost:8000",{
+            const socket = io("https://chat-app-eoi2.onrender.com",{
                 query:{
                     userId:authUser._id
                 }
